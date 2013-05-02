4d04e26fb58b991a150228236878c82ee9f53bc2
brian <none>
2013-05-02 18:35:00 -0400

    Test commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:username], params[:password])
     if user
       session[:user_id] = user.id
-      if user.user_type == "shopper"
-        redirect_to articles_path, :notice => "Logged in!"
+	  redirect_to articles_path, :notice => "Logged in!"
       end
 	  
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,65 +12,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # orders can only be processed in the non-saved stated.
-  def process_orders
-    @items = Item.find(:all, :conditions => ["buyer=? and saved=?",current_user.username,false])
-    Item.setToOrdered(@items)
-    redirect_to view_cart_path
-  end
-
-  def view_orders
-    @items = Item.find(:all, :conditions => ["seller=? and order_id=?",current_user.username, 1])
-    respond_to do |format|
-      format.html # view_orders.html.erb
-      format.json { render json: @items }
-    end
-  end
-
-  def keeper_home
-    @items = Item.find(:all, :conditions => ["seller=? and buyer=? and count=?",current_user.username, "none", 1])
-    respond_to do |format|
-      format.html # keeper_home.html.erb
-      format.json { render json: @items }
-    end
-  end
-
-  def view_cart
-    @items = Item.find(:all, :conditions => ["buyer=? and order_id=? and saved=?",current_user.username, 0, false])
-    respond_to do |format|
-      format.html # view_cart.html.erb
-      format.json { render json: @items }
-    end
-  end
-
-  # Allows a shopper to create a copy of the keeper's item.
-  def addToCart
-    item = Item.find(params[:id])
-    @item = Item.new(:name => item.name, :price => item.price, :seller => item.seller, :buyer => current_user.username, :count => 1, :order_id => 0, :saved => item.saved)
-    @item.save
-    if @item.save
-      redirect_to items_path, :notice => "Item added to Cart"
-    else
-      render "new"
-    end
-  end
-
-  def displaySavedItems
-    @items = Item.find(:all, :conditions => ["buyer=? and saved=?",current_user.username, true])
-    respond_to do |format|
-      format.html # saved.html.erb
-      format.json { render json: @users }
-    end
-  end
-
-  def save_cart
-    @items = Item.find(:all, :conditions => ["buyer=? and order_id=? and saved=?",current_user.username, 0, false])
-    @items.each do |item|
-      Item.saveItem(item)
-    end
-    redirect_to view_cart_path, :notice => 'Entire cart saved'
-  end
-
   def update
     @user = User.find(params[:id])
     respond_to do |format|
