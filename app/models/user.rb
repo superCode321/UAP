8500b677942c9b22b736d1b5f18f0034402e07a7
brian <none>
2013-05-02 18:45:39 -0400

    changed sessions_controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_presence_of :username
-  validates_presence_of :user_type
   validates_uniqueness_of :username
 
 
