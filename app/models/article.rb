128e5cf41c7a6712513b0e6b8aa8fe3920261c30
brian <none>
2013-05-14 01:53:30 -0400

    fixed models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
+	has_one :karticle
 	attr_accessible :title, :body, :source_url
 end
--- app/models/karticle.rb
+++ app/models/karticle.rb
@@ -1,3 +1,4 @@
 class Karticle < ActiveRecord::Base
+	belongs_to :article
 	attr_accessible :article_id, :score
 end
