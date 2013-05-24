dd6d39a184a26c655063fabbf31e0b3babcf96e5
brian <none>
2013-05-24 08:56:34 -0400

    Fixing schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130524121619) do
+ActiveRecord::Schema.define(:version => 20130524124345) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -48,10 +48,10 @@ ActiveRecord::Schema.define(:version => 20130524121619) do
   end
 
   create_table "words", :force => true do |t|
-    t.text     "text",       :limit => 255
+    t.string   "text"
     t.integer  "difficulty"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
